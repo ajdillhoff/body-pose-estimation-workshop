609afe3af5fcad71c345d54503f3daadac4b0787
Alex Dillhoff <ajdillhoff@gmail.com>
2023-10-07 13:29:27 -0500

    Fixed requirements.

--- DemocraTunes/audio.py
+++ DemocraTunes/audio.py
@@ -14,7 +14,7 @@ class SpotifyPlayer:
         try:
             spotipy_client_id = os.environ["SPOTIPY_CLIENT_ID"]
             spotipy_client_secret = os.environ["SPOTIPY_CLIENT_SECRET"]
-            spotipy_redirect_uri = os.environ["SPOTIPY_REDIRECT_URI", "http://localhost:8080"]
+            spotipy_redirect_uri = os.environ.get("SPOTIPY_REDIRECT_URI", "http://localhost:8080")
         except KeyError:
             print("Please set the SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, and SPOTIPY_REDIRECT_URI environment variables.")
             exit(1)
